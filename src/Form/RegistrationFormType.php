d1fdf0a31dd55c76f9ff7fcc7ace7fda058a2980
Nabuko81 <mathieubrachet@hotmail.com>
2020-01-27 15:45:37 +0100

    User sans recup mot de passe

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,9 +11,10 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -21,44 +22,53 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-                'label' => 'Email'
             ])
 
             ->add('firstName', TextType::class, [
-                'label' => 'Prénom'
+                // 'label' => 'Prénom'
             ])
 
             ->add('lastName', TextType::class, [
-                'label' => 'Nom'
+                // 'label' => 'Nom'
             ])
 
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'label' => 'Merci d\'accepter les conditions',
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Vous devez accepter les conditions',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'label' => 'Mot de passe',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Tapez un mot de passe svp',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'label' => 'Merci d\'accepter les conditions',
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'Vous devez accepter les conditions',
+            //         ]),
+            //     ],
+            // ])
+
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Merci d\'indiquer des mots de passes identiques.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options' => [],
+                'second_options'=> [],
+                
+            ]);
+            // ->add('plainPassword', PasswordType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'label' => 'Mot de passe',
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Tapez un mot de passe svp',
+            //         ]),
+                    // new Length([
+                    //     'min' => 6,
+                    //     'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                    //     // max length allowed by Symfony for security reasons
+                    //     'max' => 4096,
+                    // ]),
+            //     ],
+            // ])
+        
     }
 
     public function configureOptions(OptionsResolver $resolver)
